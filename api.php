e10cd8fbcc372de8e2ea1e00a904462f8e28046b
n.brovkovich <n.brovkovich@rsit.ru>
2020-02-08 14:15:14 +0400

    api optimise

--- api.php
+++ api.php
@@ -1,109 +1,89 @@
 <?php
 
-$RESULT = [];
-
 $DB = new PDO('mysql:host=localhost;dbname=worldapi', 'mysql', 'mysql');
 
+$tables = [
+    'users' => ['login', 'password', 'created_at', 'updated_at', 'deleted_at'],
+];
 
-function getUsers() {
-    global $DB;
-
-    $query = $DB->prepare('SELECT * FROM users WHERE deleted_at = "0"');
+$RESULT = [];
+foreach ($tables as $tableName => $value) {
+    $query = $DB->prepare("SELECT * FROM {$tableName} WHERE deleted_at = '0'");
     $query->execute();
-    $rows = $query->fetchAll(PDO::FETCH_ASSOC);
-
-    return $rows;
+    $RESULT[$tableName] = $query->fetchAll(PDO::FETCH_ASSOC);
 }
 
-$RESULT['users'] = getUsers();
+if (isset($_GET['api']) && $_GET['api'] === 'all') {
 
-if(isset($_GET['api'])) {
-    if($_GET['api'] === 'all') {
-
-        header('HTTP/1.1 200 OK');
-        header('Content-Type: application/json;');
-        print_r(json_encode($RESULT));
-    }
+    header('HTTP/1.1 200 OK');
+    header('Content-Type: application/json;');
+    print_r(json_encode($RESULT));
+    die();
 }
 
 
-
-if(isset($_POST['api'])) {
+if (isset($_POST['api']) && $_POST['api'] === 'update') {
     header('HTTP/1.1 200 OK');
     header('Content-Type: application/json;');
 
-    if($_POST['api'] === 'update') {
+    $data = json_decode($_POST['data'], true);
 
-        $tables = [
-            'users' => [
-                'login' => '',
-                'password' => '',
-                'created_at' => '',
-                'updated_at' => '',
-                'deleted_at' => '',
-            ]
-        ];
+    if (isset($data) && is_array($data)) {
+        foreach ($data as $tableName => $rows) {
 
-        $sqlToUpdate = [
-            'users' => 'UPDATE users SET login=:login, password=:password, updated_at = :updated_at WHERE id=:id',
-        ];
+            foreach ($rows as $row) {
+                if (isset($row['created_at']) && $row['created_at'] === 1) {
+                    // create row
 
+                    $row['created_at'] = mktime();
 
-        $data = json_decode($_POST['data'], true);
-        if(isset($data) && is_array($data)) {
-            foreach ($data as $tableName => $rows) {
-
-                foreach ($rows as $row) {
-
-                    if(isset($row['created_at']) && $row['created_at'] === 1) {
-                        $row['created_at'] = mktime();
-
-                        // prepare data
-                        $fields = [];
-                        $fieldKeys = [];
-                        $fieldValues = [];
-                        foreach ($tables[$tableName] as $key => $item) {
-                            if(isset($row[$key])) {
-                                $fields[':' . $key] = $row[$key];
-                                $fieldKeys[] = $key;
-                                $fieldValues[] = ':' . $key;
-                            }
+                    // prepare data
+                    $fields = [];
+                    $fieldKeys = [];
+                    $fieldValues = [];
+                    foreach ($tables[$tableName] as $columnName) {
+                        if (isset($row[$columnName])) {
+                            $fields[':' . $columnName] = $row[$columnName];
+                            $fieldKeys[] = $columnName;
+                            $fieldValues[] = ':' . $columnName;
                         }
-
-                        // form sql
-                        $strKeys = join(',', $fieldKeys);
-                        $strValues = join(',', $fieldValues);
-                        $strSql = "INSERT INTO {$tableName} ({$strKeys}) VALUES ({$strValues})";
-
-                        $sql = $DB->prepare($strSql);
-                        $result = $sql->execute($fields);
                     }
 
-                    if(isset($row['updated_at']) && $row['updated_at'] === 1) {
-                        $row['updated_at'] = mktime();
+                    // form sql
+                    $strKeys = join(',', $fieldKeys);
+                    $strValues = join(',', $fieldValues);
+                    $strSql = "INSERT INTO {$tableName} ({$strKeys}) VALUES ({$strValues})";
 
-                        // prepare data
-                        $fields = [];
-                        $fieldKeys = [];
-                        foreach ($tables[$tableName] as $key => $item) {
-                            if(isset($row[$key])) {
-                                $fields[':' . $key] = $row[$key];
-                                $fieldKeys[] = $key . '=' . ':' . $key;
-                            }
-                        }
-
-                        // form sql
-                        $strKeys = join(',', $fieldKeys);
-                        $strSql = "UPDATE {$tableName} SET {$strKeys} WHERE id=:id";
-
-                        $sql = $DB->prepare($strSql);
-                        $result = $sql->execute($fields);
-                    }
-
-
+                    $sql = $DB->prepare($strSql);
+                    $result = $sql->execute($fields);
                 }
+
+                if (isset($row['updated_at']) && $row['updated_at'] === 1) {
+                    // update row
+
+                    $row['updated_at'] = mktime();
+
+                    // prepare data
+                    $fields = [];
+                    $fieldKeys = [];
+                    foreach ($tables[$tableName] as $columnName) {
+                        if (isset($row[$columnName])) {
+                            $fields[':' . $columnName] = $row[$columnName];
+                            $fieldKeys[] = $columnName . '=' . ':' . $columnName;
+                        }
+                    }
+
+                    // form sql
+                    $strKeys = join(',', $fieldKeys);
+                    $strSql = "UPDATE {$tableName} SET {$strKeys} WHERE id=:id";
+
+                    $sql = $DB->prepare($strSql);
+                    $result = $sql->execute($fields);
+                }
+
+
             }
         }
-        print_r(json_encode(['status' => true]));
     }
+    print_r(json_encode(['status' => true]));
 }
